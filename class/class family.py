1a8cd943657136750ba3b576541fee7a45efd006
NathMTK <nathanowenmateke@gmail.com>
2025-12-08 15:16:58 +0100

    nouv

--- class/class family.py
+++ class/class family.py
@@ -1,9 +1,27 @@
 #1 creer une class membre de la famille
 class familyMember ():
     def __init__(self,name,firstName,bornDate,Tree):
+    def __init__(self,name,firstName,bornDate):
         self.name = name
         self.firstName = firstName
         self.bornDate = bornDate
         self.id = parent_id
     def showParent(id):
         father = Tree
+
+#2 organisation en arbre
+class Node:
+    def __init__(self, valeur):
+        self.valeur = valeur
+        self.pere = None
+        self.mere = None
+
+
+class ABR:
+    def __init__(self):
+        self.enfant = None
+#test
+myTree = ABR()
+myTree.enfant = familyMember("Ethan","Durvin","13/09/2008")
+myTree.enfant.pere = familyMember("jean","Durvin","06/07/1985")
+myTree.enfant.pere.pere = familyMember("David","Durvin","03/08/1930")
