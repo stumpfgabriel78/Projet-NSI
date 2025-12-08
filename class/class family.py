8e37dc948f24f3ffbc41a73bb15d421b4b91167d
NathMTK <nathanowenmateke@gmail.com>
2025-12-08 14:59:51 +0100

    class family

---
+++ class/class family.py
@@ -0,0 +1,9 @@
+#1 creer une class membre de la famille
+class familyMember ():
+    def __init__(self,name,firstName,bornDate,Tree):
+        self.name = name
+        self.firstName = firstName
+        self.bornDate = bornDate
+        self.id = parent_id
+    def showParent(id):
+        father = Tree
