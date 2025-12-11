83712c0566bc0488525f6d38741d44e9a533f22b
NathMTK <nathanowenmateke@gmail.com>
2025-12-11 16:03:15 +0100

    eneleve les tests

--- class/class family.py
+++ class/class family.py
@@ -17,8 +17,3 @@ class Node:
 class ABR:
     def __init__(self):
         self.enfant = None
-#test
-myTree = ABR()
-myTree.enfant = familyMember("Ethan","Durvin","13/09/2008")
-myTree.enfant.pere = familyMember("jean","Durvin","06/07/1985")
-myTree.enfant.pere.pere = familyMember("David","Durvin","03/08/1930")
