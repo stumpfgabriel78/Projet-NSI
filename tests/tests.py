2f93d0c61ba0fc9af7b10e006ab863a667b987e8
NathMTK <nathanowenmateke@gmail.com>
2026-01-08 15:45:46 +0100

    Delete tests directory

--- tests/tests.py
+++
@@ -1,5 +0,0 @@
-#test
-myTree = ABR()
-myTree.enfant = familyMember("Ethan","Durvin","13/09/2008")
-myTree.enfant.pere = familyMember("jean","Durvin","06/07/1985")
-myTree.enfant.pere.pere = familyMember("David","Durvin","03/08/1930")
